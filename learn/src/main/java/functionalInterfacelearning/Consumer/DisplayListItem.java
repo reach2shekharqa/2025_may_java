a753d70af5061b753530493374ef1a859846b2d7
Shekhar <reach2shekhar@gmail.com>
2025-05-02 10:28:21 +0530

    Refactor DisplayListItem to enhance book sorting and filtering functionality 📚✨

--- learn/src/main/java/functionalInterfacelearning/Consumer/DisplayListItem.java
+++ learn/src/main/java/functionalInterfacelearning/Consumer/DisplayListItem.java
@@ -1,5 +1,6 @@
 package functionalInterfacelearning.Consumer;
 
+import java.util.Collections;
 import java.util.List;
 
 import java8learning.BookDao;
@@ -10,10 +11,25 @@ public class DisplayListItem {
     public static void main(String[] args) {
 
         BookDao obj = new BookDao();
-        
-        List<Books> list= obj.getBooks();
 
-        list.forEach(item->System.out.println(item));
+        List<Books> listOfBooks = obj.getBooks();
+
+        System.out.println("============comparator example==============");
+
+        // Sorting the list of books in ascending order based on their page count using a lambda expression.
+        Collections.sort(listOfBooks, (obj1, obj2) -> obj1.getPageCount().compareTo(obj2.getPageCount()));
+        System.out.println("after sorting(asc order)...." + listOfBooks);
+
+        System.out.println("============consumer example==============");
+
+        // Using a Consumer functional interface to iterate through the list of books
+        // and print each book's details to the console.
+        listOfBooks.forEach(item -> System.out.println(item));
+
+        // Filtering books with a page count greater than or equal to 222 using a predicate
+        listOfBooks.stream().filter(book -> book.getPageCount() >= 222).forEach(b -> System.out.println(b));
+
+        listOfBooks.stream().filter(book -> book.getPageCount() >= 222).forEach(b -> System.out.println(b)); // predicate
+
     }
-
 }
