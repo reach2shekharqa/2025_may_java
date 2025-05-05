8df454946c985830fe89682a34e6fb7767fcd9df
reach2shekharqa <reach2shekharqa@gmail.com>
2025-05-05 11:23:07 +0530

    Refactor FindDistinct class to improve sorting logic and comment out unused stream operations

--- learn/src/main/java/javastream05thmay/FindDistinct.java
+++ learn/src/main/java/javastream05thmay/FindDistinct.java
@@ -11,18 +11,22 @@ public class FindDistinct {
 
         Supplier<Stream<String>> supplierList = ()-> Stream.of("shekhar","rahul","sss","shekhar");
 
-       System.out.println( supplierList.get()
-        .distinct()
-        .collect(Collectors.toList()));
+    //    System.out.println( supplierList.get()
+    //     .distinct()
+    //     .collect(Collectors.toList()));
 
 
-        System.out.println( supplierList.get()
-        .sorted()
-        .collect(Collectors.toList()));
+    //     System.out.println( supplierList.get()
+    //     .sorted()
+    //     .collect(Collectors.toList()));
 
         Integer[] arr = {11,4,6,8,9};
 
-        System.out.println(Arrays.stream(arr).sorted().collect(Collectors.toList()));
+        System.out.println(Arrays.
+        stream(arr)
+        .sorted((a,b)-> b.compareTo(a))
+        
+        .collect(Collectors.toList()));
 
 
 
