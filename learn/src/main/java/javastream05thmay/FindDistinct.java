66b362b4de5271cef579c645861d238eb6c184c2
reach2shekharqa <reach2shekharqa@gmail.com>
2025-05-05 14:53:25 +0530

    Comment out unused code in FindDistinct class and add new classes for examples with comments and emojis

--- learn/src/main/java/javastream05thmay/FindDistinct.java
+++ learn/src/main/java/javastream05thmay/FindDistinct.java
@@ -7,28 +7,24 @@ public class FindDistinct {
 
     public static void main(String[] args) {
 
-        // Supplier<Stream<String>> supplierList = ()-> Stream.of("shekhar","rahul","sss","shekhar");
+        // Supplier<Stream<String>> supplierList = ()->
+        // Stream.of("shekhar","rahul","sss","shekhar");
 
-    //    System.out.println( supplierList.get()
-    //     .distinct()
-    //     .collect(Collectors.toList()));
+        // System.out.println( supplierList.get()
+        // .distinct()
+        // .collect(Collectors.toList()));
 
+        // System.out.println( supplierList.get()
+        // .sorted()
+        // .collect(Collectors.toList()));
 
-    //     System.out.println( supplierList.get()
-    //     .sorted()
-    //     .collect(Collectors.toList()));
+        Integer[] arr = { 11, 4, 6, 8, 9 };
 
-        Integer[] arr = {11,4,6,8,9};
+        System.out.println(Arrays.stream(arr)
+                .sorted((a, b) -> b.compareTo(a))
 
-        System.out.println(Arrays.
-        stream(arr)
-        .sorted((a,b)-> b.compareTo(a))
-        
-        .collect(Collectors.toList()));
+                .collect(Collectors.toList()));
 
-
-
-        
     }
-    
+
 }
